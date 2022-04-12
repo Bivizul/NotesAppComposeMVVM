020f817102aa7f660095ced0c7108ff2da55779d
Bivizul <mamochapa@gmail.com>
2022-04-12 17:00:18 +0200

    Layout screens Navigation between screens

--- app/src/main/java/com/bivizul/notesappcomposemvvm/screens/AddScreen.kt
+++ app/src/main/java/com/bivizul/notesappcomposemvvm/screens/AddScreen.kt
@@ -1,9 +1,70 @@
 package com.bivizul.notesappcomposemvvm.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.bivizul.notesappcomposemvvm.navigation.NavRoute
+import com.bivizul.notesappcomposemvvm.ui.theme.NotesAppComposeMVVMTheme
 
 // Создаем экран добавления
 @Composable
 fun AddScreen(navController: NavHostController) {
+
+    var title by remember { mutableStateOf("") }
+    var subtitle by remember { mutableStateOf("") }
+
+    Scaffold {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Add new note",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            OutlinedTextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text(text = "Note title") }
+            )
+            OutlinedTextField(
+                value = subtitle,
+                onValueChange = { subtitle = it },
+                label = { Text(text = "Note subtitle") }
+            )
+            Button(
+                modifier = Modifier.padding(top = 16.dp),
+                onClick = {
+                    navController.navigate(route = NavRoute.MainScreen.route)
+                }
+            ) {
+                Text(text = "Add note")
+            }
+
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun previewAddScreen() {
+    NotesAppComposeMVVMTheme {
+        AddScreen(navController = rememberNavController())
+    }
 }
--- app/src/main/java/com/bivizul/notesappcomposemvvm/screens/StartScreen.kt
+++ app/src/main/java/com/bivizul/notesappcomposemvvm/screens/StartScreen.kt
@@ -1,11 +1,60 @@
 package com.bivizul.notesappcomposemvvm.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.bivizul.notesappcomposemvvm.navigation.NavRoute
+import com.bivizul.notesappcomposemvvm.ui.theme.NotesAppComposeMVVMTheme
 
 // Создаем стартовый экран
 @Composable
 fun StartScreen(navController: NavHostController) {
-    Text(text = "Start Screen")
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "What will we use?")
+            Button(
+                onClick = {
+                    navController.navigate(route = NavRoute.MainScreen.route)
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(vertical = 8.dp)
+            ) {
+                Text(text = "Room database")
+            }
+            Button(
+                onClick = {
+                    navController.navigate(route = NavRoute.MainScreen.route)
+                },
+                modifier = Modifier
+                    .width(200.dp)
+                    .padding(vertical = 8.dp)
+            ) {
+                Text(text = "Firebase database")
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun previewStartScreen() {
+    NotesAppComposeMVVMTheme {
+        StartScreen(navController = rememberNavController())
+    }
 }
